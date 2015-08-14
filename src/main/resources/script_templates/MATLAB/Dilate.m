1f4c69af1390c39f93af5617712fb34b92503569
Mark Hiner <hinerm@gmail.com>
2015-08-14 11:07:42 -0500

    Dilate.m: add return value

--- src/main/resources/script_templates/MATLAB/Dilate.m
+++ src/main/resources/script_templates/MATLAB/Dilate.m
@@ -1,9 +1,10 @@
 % @matrix data
+% @OUTPUT net.imagej.Dataset rval
 
 % Performs dilation on the active dataset with a preset structure
 
-data = uint8(data); % convert to uint8
-mask = im2bw(data,0.5); % make bw mask
+rval = uint8(data); % convert to uint8
+mask = im2bw(rval,0.5); % make bw mask
 se = strel('line',4,180); % create structure to use in dilation
 mask = imdilate(mask,se); % perform dilation on the mask
-data(~mask) = 0; % subtract mask from original dataset
+rval(~mask) = 0; % subtract mask from original dataset
